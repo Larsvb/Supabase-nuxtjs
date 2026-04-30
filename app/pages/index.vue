ce2034794aaf6ebd292f8bfe980c6e0cf9674d65
Lars van Boven <lars.vanboven@radancy.com>
2026-04-30 09:36:28 +0200

    update project folder structure

---
+++ app/pages/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+  import Hero from '~/components/Hero.vue'
+  import About from '~/components/About.vue'
+</script>
+
+<template>
+  <div class="page">
+    <Hero />
+    <About />
+  </div>
+</template>
+
+<style scoped>
+.page {
+  width: 100%;
+  min-height: 100vh;
+}
+</style>
